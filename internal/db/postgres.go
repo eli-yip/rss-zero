5151090b8f2e3cee940b38b7f2e4a5c92e878e3a
Eli Yip <yezi.2022@outlook.com>
2024-01-11 18:21:57 +0800

    feat: add db

---
+++ internal/db/postgres.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func NewDB(host, port, user, password, name string) (db *gorm.DB, err error) {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Silent,
+			Colorful:      false,
+		},
+	)
+
+	mdsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Asia/Shanghai", host, port, user, password, name)
+	if db, err = gorm.Open(postgres.Open(mdsn), &gorm.Config{
+		PrepareStmt: true,
+		Logger:      newLogger,
+	}); err != nil {
+		panic(err)
+	}
+	mdb, _ := db.DB()
+	mdb.SetMaxIdleConns(20)
+	mdb.SetMaxOpenConns(100)
+	mdb.SetConnMaxLifetime(time.Hour)
+
+	return db, nil
+}
