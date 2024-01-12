c51c31420e460f0d681957567dcfc2b0fc152ab9
Eli Yip <yezi.2022@outlook.com>
2024-01-12 14:20:54 +0800

    enh: use better layout of log time

--- pkg/log/zap.go
+++ pkg/log/zap.go
@@ -4,10 +4,14 @@ import (
 	"fmt"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func NewLogger() *zap.Logger {
-	logger, err := zap.NewProduction()
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("2006-01-02 15:04:05")
+
+	logger, err := config.Build()
 	if err != nil {
 		panic(fmt.Sprintf("Failed to init logger: %v", err))
 	}
