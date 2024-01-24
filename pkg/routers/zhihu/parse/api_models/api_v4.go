5969b236dc28b9391238a762e0785f07dc28f70d
Eli Yip <yezi.2022@outlook.com>
2024-01-24 23:59:04 +0800

    feat: complete apiv4 and xml answer parse struct

---
+++ pkg/routers/zhihu/parse/api_models/api_v4.go
@@ -0,0 +1,23 @@
+package apiModel
+
+// V4InitalState represents json from appview/api/v4/answers/{answerId}
+type V4Answer struct {
+	ID          string   `json:"id"`
+	CreatedTime int      `json:"created_time"`
+	Content     string   `json:"content"`
+	Author      Author   `json:"author"`
+	Question    Question `json:"question"`
+}
+
+// Author is shared by V4Answer and XmlAnswer
+type Author struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// Question is shared by V4Answer and XmlAnswer
+type Question struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	CreatedTime int    `json:"created"`
+}
---
+++ pkg/routers/zhihu/parse/api_models/xml.go
@@ -0,0 +1,18 @@
+package apiModel
+
+// XMLInitalState represents json from xml <body><script id="js-initialData" type="text/json">...</script></body>
+type XmlInitalData struct {
+	InitialState struct {
+		Entities struct {
+			Answers map[string]XmlAnswer `json:"answers"`
+		} `json:"entities"`
+	} `json:"initialState"`
+}
+
+type XmlAnswer struct {
+	ID          int      `json:"id"`
+	Content     string   `json:"content"`
+	CreatedTime int      `json:"createdTime"`
+	Author      Author   `json:"author"`
+	Question    Question `json:"question"`
+}
