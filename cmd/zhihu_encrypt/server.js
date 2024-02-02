aa3604c04881e0974f35d96518b03953dfe4c712
Eli Yip <yezi.2022@outlook.com>
2024-02-02 23:21:55 +0800

    enh: add log to zhihu encrypt server

--- cmd/zhihu_encrypt/server.js
+++ cmd/zhihu_encrypt/server.js
@@ -7,6 +7,7 @@ app.use(express.json());
 app.post('/encrypt', (req, res) => {
   const cookieMes = req.body.cookie_mes;
   const apiPath = req.body.api_path;
+  console.log('receive api_path:', apiPath);
 
   if (!cookieMes || !apiPath) {
     console.log('Missing parameters');
@@ -14,6 +15,7 @@ app.post('/encrypt', (req, res) => {
   }
 
   xzse96 = calculateXZSE96(apiPath, cookieMes)
+  console.log('figure out xzse96:', xzse96);
   res.send({ xzse96 });
 });
 
