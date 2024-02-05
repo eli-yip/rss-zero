21083e10e0a23e3c3538ebb24b715516c3f12c8a
Eli Yip <yezi.2022@outlook.com>
2024-02-05 16:32:12 +0800

    feat: support author info

---
+++ pkg/routers/zhihu/parse/author.go
@@ -0,0 +1,26 @@
+package parse
+
+import (
+	"encoding/json"
+
+	"github.com/eli-yip/rss-zero/pkg/routers/zhihu/db"
+	apiModels "github.com/eli-yip/rss-zero/pkg/routers/zhihu/parse/api_models"
+)
+
+func (p *Parser) ParseAuthorName(content []byte) (authorName string, err error) {
+	p.logger.Info("start to parse author name")
+
+	var author apiModels.Author
+	if err = json.Unmarshal(content, &author); err != nil {
+		return "", err
+	}
+
+	if err = p.db.SaveAuthor(&db.Author{
+		ID:   author.ID,
+		Name: author.Name,
+	}); err != nil {
+		return "", err
+	}
+
+	return author.Name, nil
+}
