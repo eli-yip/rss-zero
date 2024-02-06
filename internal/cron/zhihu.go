ff2e4213918bfc0b73cfa944e3c2613622165f7f
Eli Yip <yezi.2022@outlook.com>
2024-02-06 14:43:23 +0800

    enh: add better log and comment to crawl zhihu cron

--- internal/cron/zhihu.go
+++ internal/cron/zhihu.go
@@ -34,125 +34,168 @@ func CrawlZhihu(redisService *redis.RedisService, db *gorm.DB, notifier notify.N
 
 		requestService, err := request.NewRequestService(nil, logger)
 		if err != nil {
-			logger.Error("failed to create request service", zap.Error(err))
+			logger.Error("fail to create request service", zap.Error(err))
 			return
 		}
+		logger.Info("zhihu request service initialized")
 
 		fileService, err := file.NewFileServiceMinio(config.C.MinioConfig, logger)
 		if err != nil {
-			logger.Error("failed to create file service", zap.Error(err))
+			logger.Error("fail to create file service", zap.Error(err))
 			return
 		}
+		logger.Info("zhihu file service initialized")
 
 		htmlToMarkdown := render.NewHTMLToMarkdownService(logger)
+		logger.Info("zhihu html to markdown service initialized")
 
 		imageParser := parse.NewImageParserOnline(requestService, fileService, dbService, logger)
+		logger.Info("zhihu image parser initialized")
+
 		parser := parse.NewParser(htmlToMarkdown, requestService, fileService, dbService, imageParser, logger)
+		logger.Info("zhihu parser initialized")
 
 		subs, err := dbService.GetSubs()
 		if err != nil {
-			logger.Error("failed to get subs", zap.Error(err))
+			logger.Error("fail to get subs", zap.Error(err))
 			return
 		}
+		logger.Info("subs fetched")
 
 		for _, sub := range subs {
-			ts := getSubType(sub.Type)
-			logger := logger.With(zap.String("author id", sub.AuthorID),
-				zap.String("type", ts))
+			ts := getSubType(sub.Type) // type in string
+			logger := logger.With(zap.String("author id", sub.AuthorID), zap.String("type", ts))
 			logger.Info("start to crawl")
 
 			switch ts {
 			case "answer":
+				// get answers from db to check if there is any answer for this sub
 				answers, err := dbService.GetLatestNAnswer(1, sub.AuthorID)
 				if err != nil {
 					logger.Error("failed to get latest answer", zap.Error(err))
 					continue
 				}
+
 				if len(answers) == 0 {
-					logger.Info("no answer found")
+					logger.Info("no answer found in db, start to crawl answer in one time mode")
+					// set target time to long long ago, as one time mode is enabled, this will not cause bugs
+					// enable one time mode as we do not know latest time in db(no answer found in db)
+					// set offset to 0 to disable backtrack mode
 					if err = crawl.CrawlAnswer(sub.AuthorID, requestService, parser, longLongago, 0, true, logger); err != nil {
 						logger.Error("failed to crawl answer", zap.Error(err))
 						continue
 					}
 				} else {
+					// set target time to the latest answer's create time in db
+					// disable one time mode, as we know when to stop(latest answer's create time)
+					// set offset to 0 to disable backtrack mode
 					if err = crawl.CrawlAnswer(sub.AuthorID, requestService, parser, answers[0].CreateAt, 0, false, logger); err != nil {
 						logger.Error("failed to crawl answer", zap.Error(err))
 						continue
 					}
 				}
+				logger.Info("crawl answer done")
 
 				path, content, err := rss.GenerateZhihu(rss.TypeAnswer, sub.AuthorID, dbService)
 				if err != nil {
 					logger.Error("failed to generate rss", zap.Error(err))
 					continue
 				}
+				logger.Info("rss generated")
 
 				if err := redisService.Set(path, content, rssTTL); err != nil {
 					logger.Error("failed to set rss to redis", zap.Error(err))
 				}
+				logger.Info("rss saved to redis")
 			case "article":
+				// get articles from db to check if there is any article for this sub
 				articles, err := dbService.GetLatestNArticle(1, sub.AuthorID)
 				if err != nil {
 					logger.Error("failed to get latest article", zap.Error(err))
 					continue
 				}
+
 				if len(articles) == 0 {
-					logger.Info("no article found")
+					logger.Info("no article found in db, start to crawl article in one time mode")
+					// set target time to long long ago, as one time mode is enabled, this will not cause bugs
+					// enable one time mode as we do not know latest time in db(no article found in db)
+					// set offset to 0 to disable backtrack mode
 					if err = crawl.CrawlArticle(sub.AuthorID, requestService, parser, longLongago, 0, true, logger); err != nil {
 						logger.Error("failed to crawl article", zap.Error(err))
 						continue
 					}
 				} else {
+					logger.Info("found article in db, start to crawl article in normal mode",
+						zap.Time("latest article's create time", articles[0].CreateAt))
+					// set target time to the latest article's create time in db
+					// disable one time mode, as we know when to stop(latest article's create time)
+					// set offset to 0 to disable backtrack mode
 					if err = crawl.CrawlArticle(sub.AuthorID, requestService, parser, articles[0].CreateAt, 0, false, logger); err != nil {
 						logger.Error("failed to crawl article", zap.Error(err))
 						continue
 					}
 				}
+				logger.Info("crawl article done")
 
 				path, content, err := rss.GenerateZhihu(rss.TypeArticle, sub.AuthorID, dbService)
 				if err != nil {
 					logger.Error("failed to generate rss", zap.Error(err))
 					continue
 				}
+				logger.Info("rss generated")
 
 				if err := redisService.Set(path, content, rssTTL); err != nil {
 					logger.Error("failed to set rss to redis", zap.Error(err))
 				}
+				logger.Info("rss saved to redis")
 			case "pin":
+				// get pins from db to check if there is any pin for this sub
 				pins, err := dbService.GetLatestNPin(1, sub.AuthorID)
 				if err != nil {
 					logger.Error("failed to get latest pin", zap.Error(err))
 					continue
 				}
+
 				if len(pins) == 0 {
-					logger.Info("no pin found")
+					logger.Info("no pin found in db, start to crawl pin in one time mode")
+					// set target time to long long ago, as one time mode is enabled, this will not cause bugs
+					// enable one time mode as we do not know latest time in db(no pin found in db)
+					// set offset to 0 to disable backtrack mode
 					if err = crawl.CrawlPin(sub.AuthorID, requestService, parser, longLongago, 0, true, logger); err != nil {
 						logger.Error("failed to crawl pin", zap.Error(err))
 						continue
 					}
 				} else {
+					logger.Info("found pin in db, start to crawl pin in normal mode",
+						zap.Time("latest pin's create time", pins[0].CreateAt))
+					// set target time to the latest pin's create time in db
+					// disable one time mode, as we know when to stop(latest pin's create time)
+					// set offset to 0 to disable backtrack mode
 					if err = crawl.CrawlPin(sub.AuthorID, requestService, parser, pins[0].CreateAt, 0, false, logger); err != nil {
 						logger.Error("failed to crawl pin", zap.Error(err))
 						continue
 					}
 				}
+				logger.Info("crawl pin done")
 
 				path, content, err := rss.GenerateZhihu(rss.TypePin, sub.AuthorID, dbService)
 				if err != nil {
 					logger.Error("failed to generate rss", zap.Error(err))
 					continue
 				}
+				logger.Info("rss generated")
 
 				if err := redisService.Set(path, content, rssTTL); err != nil {
 					logger.Error("failed to set rss to redis", zap.Error(err))
 				}
+				logger.Info("rss saved to redis")
 			}
 		}
 	}
 }
 
-func getSubType(subType int) string {
-	var ts string
+// convert zhihuDB.Type to string
+func getSubType(subType int) (ts string) {
 	switch subType {
 	case zhihuDB.TypeAnswer:
 		ts = "answer"
@@ -161,5 +204,6 @@ func getSubType(subType int) string {
 	case zhihuDB.TypePin:
 		ts = "pin"
 	}
+
 	return ts
 }
