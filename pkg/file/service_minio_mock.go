5ea5e6867681ada311f0f84000cb012b92e620e0
Eli Yip <yezi.2022@outlook.com>
2024-02-15 06:47:29 +0800

    refact(file): remove unused code

--- pkg/file/service_minio_mock.go
+++
@@ -1,9 +0,0 @@
-package file
-
-import "io"
-
-type MockMinio struct{}
-
-func (m *MockMinio) SaveStream(string, io.ReadCloser, int64) error { return nil }
-func (m *MockMinio) Get(string) (io.ReadCloser, error)             { return nil, nil }
-func (m *MockMinio) AssetsDomain() string                          { return "" }
