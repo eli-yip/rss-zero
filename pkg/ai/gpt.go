726905eacb924c109136beeede0b266f02dbf116
Eli Yip <yezi.2022@outlook.com>
2024-02-15 06:43:39 +0800

    chore: update models of ai package

--- pkg/ai/gpt.go
+++ pkg/ai/gpt.go
@@ -13,13 +13,10 @@ func (a *AIService) Polish(text string) (result string, err error) {
 	const polishPrompt = "请为我格式化下面的文本，使其通顺完整，谢谢！请使用Markdown格式，并且只需要回答格式化后的文本，不需要其他内容。\n\"\"\"%s\"\"\""
 
 	req := openai.ChatCompletionRequest{
-		Model: openai.GPT3Dot5Turbo1106,
+		Model: model,
 		Messages: []openai.ChatCompletionMessage{
-			{
-				Role:    openai.ChatMessageRoleUser,
-				Content: fmt.Sprintf(polishPrompt, text),
-			},
-		},
+			{Role: openai.ChatMessageRoleUser,
+				Content: fmt.Sprintf(polishPrompt, text)}},
 	}
 
 	resp, err := a.client.CreateChatCompletion(ctx, req)
@@ -36,13 +33,10 @@ func (a *AIService) Conclude(text string) (result string, err error) {
 	const concludePrompt = "请为下面的内容取一个贴近内容的标题，只需要回答标题的纯文本，不需要其他内容和任何格式。\n\"\"\"%s\"\"\""
 
 	req := openai.ChatCompletionRequest{
-		Model: openai.GPT3Dot5Turbo1106,
+		Model: model,
 		Messages: []openai.ChatCompletionMessage{
-			{
-				Role:    openai.ChatMessageRoleUser,
-				Content: fmt.Sprintf(concludePrompt, text),
-			},
-		},
+			{Role: openai.ChatMessageRoleUser,
+				Content: fmt.Sprintf(concludePrompt, text)}},
 	}
 
 	resp, err := a.client.CreateChatCompletion(ctx, req)
