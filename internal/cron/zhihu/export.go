4098ccc3cd6afd6af3c2404d72ad335ee6acac2e
Eli Yip <yezi.2022@outlook.com>
2024-06-02 20:12:28 +0800

    fix(zhihu): export date range error
    before this fix, date range is this month. After this fix, it was
    corrected to last month.

--- internal/cron/zhihu/export.go
+++ internal/cron/zhihu/export.go
@@ -6,17 +6,20 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/rs/xid"
+	"go.uber.org/zap"
+
 	zhihuController "github.com/eli-yip/rss-zero/cmd/server/controller/zhihu"
 	"github.com/eli-yip/rss-zero/internal/log"
-	"go.uber.org/zap"
 )
 
 func Export() func() {
 	return func() {
-		logger := log.NewZapLogger()
-		logger.Info("start export zhihu data")
+		logger := log.NewZapLogger().With(zap.String("export_id", xid.New().String()))
 
+		logger.Info("Start to export zhihu data of mocangli answer")
 		startDate, endDate := getStartDateEndDate(time.Now())
+		logger.Info("Get start date and end date", zap.String("start_date", startDate), zap.String("end_date", endDate))
 
 		exportReq := &zhihuController.ZhihuExportReq{
 			Author:    getStringPtr("canglimo"),
@@ -27,21 +30,23 @@ func Export() func() {
 
 		reqData, err := json.Marshal(exportReq)
 		if err != nil {
-			logger.Error("failed to marshal request data", zap.Error(err))
+			logger.Error("Failed to marshal request data", zap.Error(err))
 		}
+		logger.Info("Marshal request data successfully")
 
 		resp, err := http.Post("http://localhost:8080/api/v1/export/zhihu", "application/json", bytes.NewBuffer(reqData))
 		if err != nil {
-			logger.Error("failed to send request", zap.Error(err))
+			logger.Error("Failed to send request", zap.Error(err), zap.String("url", "http://localhost:8080/api/v1/export/zhihu"))
 		}
 		if resp.StatusCode != http.StatusOK {
-			logger.Error("failed to export zhihu data", zap.Int("status_code", resp.StatusCode))
+			logger.Error("Failed to export zhihu data, bad status code", zap.Int("status_code", resp.StatusCode))
 		}
+		logger.Info("Export zhihu data successfully")
 	}
 }
 
 func getStartDateEndDate(now time.Time) (startDate, endDate string) {
-	firstOfMonth := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
+	firstOfMonth := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location()).AddDate(0, -1, 0)
 	lastOfMonth := firstOfMonth.AddDate(0, 1, -1)
 	return firstOfMonth.Format("2006-01-02"), lastOfMonth.Format("2006-01-02")
 }
--- internal/cron/zhihu/export_test.go
+++ internal/cron/zhihu/export_test.go
@@ -8,7 +8,7 @@ import (
 func TestGetStartDateAndEndDate(t *testing.T) {
 	now := time.Date(2021, 6, 1, 0, 0, 0, 0, time.UTC)
 	startDate, endDate := getStartDateEndDate(now)
-	if startDate != "2021-06-01" || endDate != "2021-06-30" {
+	if startDate != "2021-05-01" || endDate != "2021-05-31" {
 		t.Errorf("getStartDateEndDate(now) = (%s, %s); want (2021-06-01, 2021-06-30)", startDate, endDate)
 	}
 }
