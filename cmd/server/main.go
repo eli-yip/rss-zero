7a85b6b56a16d08c0309014976cebd6f78f372e0
Eli Yip <yezi.2022@outlook.com>
2024-06-21 23:02:24 +0800

    fix(zhihu): fetch zhihu without cron

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -28,6 +28,7 @@ import (
 	"github.com/eli-yip/rss-zero/config"
 	"github.com/eli-yip/rss-zero/internal/cron"
 	xiaobotCron "github.com/eli-yip/rss-zero/internal/cron/xiaobot"
+	zhihuCron "github.com/eli-yip/rss-zero/internal/cron/zhihu"
 	zsxqCron "github.com/eli-yip/rss-zero/internal/cron/zsxq"
 	"github.com/eli-yip/rss-zero/internal/db"
 	"github.com/eli-yip/rss-zero/internal/log"
@@ -309,7 +310,7 @@ func setupCronCrawlJob(logger *zap.Logger,
 	}
 
 	dailyJobs := []crawlJob{
-		// {"zhihu_crawl", zhihuCron.Crawl},
+		{"zhihu_crawl", zhihuCron.Crawl},
 	}
 
 	for _, job := range cronCrawlJobs {
@@ -322,9 +323,9 @@ func setupCronCrawlJob(logger *zap.Logger,
 
 	for _, job := range dailyJobs {
 		jobFunc := job.fn(redisService, db, notifier)
-		if err = cronService.AddDailyCrawlJob(job.name, jobFunc); err != nil {
-			return nil, fmt.Errorf("fail to add job: %w", err)
-		}
+		// if err = cronService.AddDailyCrawlJob(job.name, jobFunc); err != nil {
+		// 	return nil, fmt.Errorf("fail to add job: %w", err)
+		// }
 		jobList = append(jobList, jobController.Job{Name: job.name, Func: jobFunc})
 	}
 
