bc4c83008cf81f4d7be5b06026949afc2e88c545
Eli Yip <yezi.2022@outlook.com>
2024-06-22 22:22:54 +0800

    feat(zhihu): set d_c0 cookie as optional

--- cmd/server/controller/zhihu/cookie.go
+++ cmd/server/controller/zhihu/cookie.go
@@ -15,7 +15,8 @@ import (
 )
 
 type SetCookieReq struct {
-	Cookie string `json:"cookie"`
+	DC0Cookie *string `json:"d_c0_cookie"`
+	ZC0Cookie *string `json:"z_c0_cookie"`
 }
 
 func (h *ZhihuController) UpdateCookie(c echo.Context) (err error) {
@@ -27,35 +28,38 @@ func (h *ZhihuController) UpdateCookie(c echo.Context) (err error) {
 		return c.JSON(http.StatusBadRequest, &common.ApiResp{Message: "invalid request"})
 	}
 
-	d_c0 := extractDC0(req.Cookie)
-	if d_c0 == "" {
-		logger.Error("Failed to extract d_c0 from cookie", zap.String("cookie", req.Cookie))
-		return c.JSON(http.StatusBadRequest, &common.ApiResp{Message: "invalid cookie"})
-	}
-	logger.Info("Retrieve zhihu d_c0 cookie successfully", zap.String("cookie", d_c0))
+	if req.DC0Cookie != nil {
+		dC0Cookie := *req.DC0Cookie
+		d_c0 := extractCookieValue(dC0Cookie)
+		if d_c0 == "" {
+			logger.Error("Failed to extract d_c0 from cookie", zap.String("cookie", dC0Cookie))
+			return c.JSON(http.StatusBadRequest, &common.ApiResp{Message: "invalid cookie"})
+		}
+		logger.Info("Retrieve zhihu d_c0 cookie successfully", zap.String("cookie", d_c0))
 
-	requestService, err := request.NewRequestService(logger, h.db, notify.NewBarkNotifier(config.C.Bark.URL), request.WithDC0(d_c0))
-	if err != nil {
-		logger.Error("Failed to create request service", zap.Error(err))
-		return c.JSON(http.StatusInternalServerError, &common.ApiResp{Message: "invalid cookie"})
-	}
+		requestService, err := request.NewRequestService(logger, h.db, notify.NewBarkNotifier(config.C.Bark.URL), request.WithDC0(dC0Cookie))
+		if err != nil {
+			logger.Error("Failed to create request service", zap.Error(err))
+			return c.JSON(http.StatusInternalServerError, &common.ApiResp{Message: "invalid cookie"})
+		}
 
-	if _, err = requestService.LimitRaw(config.C.TestURL.Zhihu, logger); err != nil {
-		logger.Error("Failed to validate zhihu d_c0 cookie", zap.Error(err))
-		return c.JSON(http.StatusInternalServerError, &common.ApiResp{Message: "invalid cookie"})
-	}
-	logger.Info("Validate zhihu d_c0 cookie successfully", zap.String("cookie", d_c0))
+		if _, err = requestService.LimitRaw(config.C.TestURL.Zhihu, logger); err != nil {
+			logger.Error("Failed to validate zhihu d_c0 cookie", zap.Error(err))
+			return c.JSON(http.StatusInternalServerError, &common.ApiResp{Message: "invalid cookie"})
+		}
+		logger.Info("Validate zhihu d_c0 cookie successfully", zap.String("cookie", d_c0))
 
-	if err = h.redis.Set(redis.ZhihuCookiePath, d_c0, redis.Forever); err != nil {
-		logger.Error("Failed to update zhihu d_c0 cookie in redis", zap.Error(err))
-		return c.JSON(http.StatusInternalServerError, &common.ApiResp{Message: err.Error()})
+		if err = h.redis.Set(redis.ZhihuCookiePath, d_c0, redis.Forever); err != nil {
+			logger.Error("Failed to update zhihu d_c0 cookie in redis", zap.Error(err))
+			return c.JSON(http.StatusInternalServerError, &common.ApiResp{Message: err.Error()})
+		}
+		logger.Info("Update zhihu d_c0 cookie in redis successfully", zap.String("cookie", d_c0))
 	}
-	logger.Info("Update zhihu d_c0 cookie in redis successfully", zap.String("cookie", d_c0))
 
 	return c.JSON(http.StatusOK, &common.ApiResp{Message: "success"})
 }
 
-func extractDC0(cookie string) (result string) {
+func extractCookieValue(cookie string) (result string) {
 	cookie = strings.TrimSpace(cookie)
 	cookie = strings.TrimSuffix(cookie, ";")
 	_, result, found := strings.Cut(cookie, "=")
