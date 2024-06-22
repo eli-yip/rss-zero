b1ea7de72fb24ce2beb3de8fccb523583982e6b6
Eli Yip <yezi.2022@outlook.com>
2024-06-23 01:01:03 +0800

    feat: remove date range in /pick API
    why: garbaled characters bug has been fixed

--- pkg/routers/zhihu/db/answer.go
+++ pkg/routers/zhihu/db/answer.go
@@ -164,8 +164,7 @@ func (d *DBService) RandomSelect(n int, userID string) (answers []Answer, err er
 
 	answerIDs := make([]int, 0, n)
 	date := time.Date(2023, 1, 1, 0, 0, 0, 0, config.C.BJT)
-	endDate := time.Date(2024, 6, 10, 0, 0, 0, 0, config.C.BJT)
-	d.Model(&Answer{}).Where("author_id = ?", userID).Where("create_at between ? and ?", date, endDate).Where("word_count between ? and ?", 300, 1200).Pluck("id", &answerIDs)
+	d.Model(&Answer{}).Where("author_id = ?", userID).Where("create_at >= ?", date).Where("word_count between ? and ?", 300, 1200).Pluck("id", &answerIDs)
 
 	if len(answerIDs) <= n {
 		if err = d.Where("id in ?", answerIDs).Find(&answers).Error; err != nil {
