d5fcf400b53c12620ef9fa3e50f261e89e7efdee
Eli Yip <yezi.2022@outlook.com>
2024-06-23 01:15:03 +0800

    feat: add id to zhihu_sub
    why: yezi/rss-zero#122

--- pkg/routers/zhihu/db/sub.go
+++ pkg/routers/zhihu/db/sub.go
@@ -3,10 +3,12 @@ package db
 import (
 	"errors"
 
+	"github.com/rs/xid"
 	"gorm.io/gorm"
 )
 
 type Sub struct {
+	ID       string `gorm:"column:id;type:string"`
 	AuthorID string `gorm:"column:author_id;type:text;primary_key"`
 	Type     int    `gorm:"column:type;type:int;primary_key"`
 	Finished bool   `gorm:"column:finished;type:boolean"`
@@ -18,11 +20,13 @@ type DBSub interface {
 	AddSub(authorID string, subType int) error
 	CheckSub(authorID string, subType int) (bool, error)
 	GetSubs() ([]Sub, error)
+	GetSubsWithNoID() ([]Sub, error)
+	SetSubID(authorID string, subType int, id string) error
 	SetStatus(authorID string, subType int, finished bool) error
 }
 
 func (d *DBService) AddSub(authorID string, subType int) error {
-	return d.Save(&Sub{AuthorID: authorID, Type: subType}).Error
+	return d.Save(&Sub{ID: xid.New().String(), AuthorID: authorID, Type: subType}).Error
 }
 
 func (d *DBService) CheckSub(authorID string, subType int) (bool, error) {
@@ -38,10 +42,23 @@ func (d *DBService) CheckSub(authorID string, subType int) (bool, error) {
 
 func (d *DBService) GetSubs() ([]Sub, error) {
 	var subs []Sub
-	if err := d.Find(&subs).Error; err != nil {
+	// order by id
+	if err := d.Order("id ASC").Find(&subs).Error; err != nil {
 		return nil, err
 	}
 	return subs, nil
+}
+
+func (d *DBService) GetSubsWithNoID() ([]Sub, error) {
+	var subs []Sub
+	if err := d.Where("id = ?", "").Find(&subs).Error; err != nil {
+		return nil, err
+	}
+	return subs, nil
+}
+
+func (d *DBService) SetSubID(authorID string, subType int, id string) error {
+	return d.Model(&Sub{}).Where("author_id = ? and type = ?", authorID, subType).Update("id", id).Error
 }
 
 func (d *DBService) SetStatus(authorID string, subType int, finished bool) error {
