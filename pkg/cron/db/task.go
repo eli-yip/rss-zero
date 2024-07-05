9685b29c8236b5ecb2bd7142746d880912112601
Eli Yip <yezi.2022@outlook.com>
2024-07-05 10:59:06 +0800

    feat: add basic CronJob interfaces
    chore: rename TaskID to TaskType, as it can descible its meaning better.

--- pkg/cron/db/task.go
+++ pkg/cron/db/task.go
@@ -6,9 +6,24 @@ type CronJob struct {
 	ID        string    `gorm:"primaryKey;column:id;type:string"`
 	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt time.Time `gorm:"column:updated_at;autoUpdateTime"`
-	TaskID    string    `gorm:"column:task_id;type:string"`
+	TaskType  string    `gorm:"column:task_type;type:string"`
 	Status    int       `gorm:"column:status;type:int"`
 	Detail    string    `gorm:"column:detail;type:string"`
 }
 
 func (*CronJob) TableName() string { return "cron_jobs" }
+
+const (
+	StatusPending = iota
+	StatusRunning
+	StatusStopped
+	StatusError
+	StatusFinished
+)
+
+type CronJobIface interface {
+	AddJob(taskType string) (taskID string, err error)
+	StopJob(taskID string) (err error)
+	UpdateStatus(taskID string, status int) (err error)
+	RecordDetail(taskID, detail string) (err error)
+}
