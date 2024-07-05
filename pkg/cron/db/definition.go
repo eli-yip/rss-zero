ad51855b1748a414044c1526a8d017e3e5a40a08
Eli Yip <yezi.2022@outlook.com>
2024-07-05 16:04:01 +0800

    feat: add cron job id to task definition

--- pkg/cron/db/definition.go
+++ pkg/cron/db/definition.go
@@ -9,21 +9,22 @@ import (
 )
 
 type CronTask struct {
-	ID        string    `gorm:"primaryKey;column:id;type:string"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt time.Time `gorm:"column:updated_at;autoUpdateTime"`
-	DeleteAt  gorm.DeletedAt
-	Type      int            `gorm:"column:type;type:int"`
-	CronExpr  string         `gorm:"column:cron_expr;type:string"`
-	Include   pq.StringArray `gorm:"column:include;type:text[]"`
-	Exclude   pq.StringArray `gorm:"column:exclude;type:text[]"`
+	ID               string    `gorm:"primaryKey;column:id;type:string"`
+	CreatedAt        time.Time `gorm:"column:created_at;autoCreateTime"`
+	UpdatedAt        time.Time `gorm:"column:updated_at;autoUpdateTime"`
+	DeleteAt         gorm.DeletedAt
+	Type             int            `gorm:"column:type;type:int"`
+	CronExpr         string         `gorm:"column:cron_expr;type:string"`
+	Include          pq.StringArray `gorm:"column:include;type:text[]"`
+	Exclude          pq.StringArray `gorm:"column:exclude;type:text[]"`
+	CronServiceJobID string         `gorm:"column:cron_service_job_id;type:string"`
 }
 
 func (*CronTask) TableName() string { return "cron_tasks" }
 
 type CronTaskIface interface {
 	AddDefinition(taskType int, cronExpr string, include, exclude []string) (id string, err error)
-	PatchDefinition(id string, cronExpr *string, include, exlucde []string) (err error)
+	PatchDefinition(id string, cronExpr *string, include, exlucde []string, cronServiceJobID *string) (err error)
 	DeleteDefinition(id string) (err error)
 	GetDefinition(id string) (task *CronTask, err error)
 	GetDefinitions() (tasks []*CronTask, err error)
@@ -46,7 +47,7 @@ func (ds *DBService) AddDefinition(taskType int, cronExpr string, include, exclu
 	}).Error
 }
 
-func (ds *DBService) PatchDefinition(id string, cronExpr *string, include, exclude []string) (err error) {
+func (ds *DBService) PatchDefinition(id string, cronExpr *string, include, exclude []string, cronServiceJobID *string) (err error) {
 	task := &CronTask{}
 	if err = ds.First(task, "id = ?", id).Error; err != nil {
 		return err
@@ -60,6 +61,10 @@ func (ds *DBService) PatchDefinition(id string, cronExpr *string, include, exclu
 	}
 	if len(exclude) > 0 {
 		task.Exclude = exclude
+	}
+
+	if cronServiceJobID != nil {
+		task.CronServiceJobID = *cronServiceJobID
 	}
 
 	return ds.Save(task).Error
