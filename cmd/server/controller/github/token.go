785e9eaff898320303acce1f522a5377474fc180
Eli Yip <yezi.2022@outlook.com>
2024-07-20 21:28:19 +0800

    fix: parse github token expire time error

--- cmd/server/controller/github/token.go
+++ cmd/server/controller/github/token.go
@@ -8,6 +8,7 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/eli-yip/rss-zero/cmd/server/controller/common"
+	"github.com/eli-yip/rss-zero/config"
 	"github.com/eli-yip/rss-zero/internal/redis"
 )
 
@@ -42,8 +43,10 @@ func (h *Controller) UpdateToken(c echo.Context) (err error) {
 		logger.Error("Error updating token", zap.Error(err))
 		return c.JSON(http.StatusBadRequest, &common.ApiResp{Message: "invalid request"})
 	}
+	t = time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, config.C.BJT)
+	t = t.Add(-24 * time.Hour)
 
-	ttl := time.Until(t.Add(-24 * time.Hour))
+	ttl := time.Until(t)
 
 	if err = h.redis.Set(redis.GitHubTokenPath, req.Token, ttl); err != nil {
 		logger.Error("Error updating token", zap.Error(err))
