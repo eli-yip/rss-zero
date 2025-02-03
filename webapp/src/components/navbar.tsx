f96c0912f81c047b603db08bd5eb7829f1ce9297
Eli Yip <yezi.2022@outlook.com>
2025-02-04 00:08:42 +0800

    fix(webapp): use same menu

--- webapp/src/components/navbar.tsx
+++ webapp/src/components/navbar.tsx
@@ -13,6 +13,7 @@ import {
 } from "@heroui/navbar";
 import { link as linkStyles } from "@heroui/theme";
 import clsx from "clsx";
+import { useLocation } from "react-router-dom";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
@@ -26,6 +27,8 @@ import {
 import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
+  const pathname = useLocation().pathname;
+
   const searchInput = (
     <Input
       aria-label="Search"
@@ -62,7 +65,7 @@ export const Navbar = () => {
         </NavbarBrand>
         <div className="hidden lg:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
+            <NavbarItem key={item.href} isActive={pathname === item.href}>
               <Link
                 className={clsx(
                   linkStyles({ color: "foreground" }),
@@ -120,17 +123,18 @@ export const Navbar = () => {
       <NavbarMenu>
         {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
+          {siteConfig.navItems.map((item, index) => (
+            <NavbarMenuItem
+              key={`${item}-${index}`}
+              isActive={pathname === item.href}
+            >
               <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
+                className={clsx(
+                  linkStyles({ color: "foreground" }),
+                  "data-[active=true]:text-primary data-[active=true]:font-medium",
+                )}
+                color="foreground"
+                href={item.href}
                 size="lg"
               >
                 {item.label}
