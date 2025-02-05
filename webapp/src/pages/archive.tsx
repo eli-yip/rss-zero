dda3cccd14e9902ecbcacf4f87355b7013645785
Eli Yip <yezi.2022@outlook.com>
2025-02-05 22:22:39 +0800

    docs(webapp): add note for pagination handling

--- webapp/src/pages/archive.tsx
+++ webapp/src/pages/archive.tsx
@@ -31,6 +31,15 @@ export default function ArchivePage() {
       {!loading && <Topics topics={topics} />}
 
       <ScrollToTop />
+      {/**
+       * Note: Why we need to check firstFetchDone here?
+       * HeroUI Pagination componet will not highlight current page in case that
+       * the total page is 0, no matter whether the total page is updated or not.
+       * So we need to make sure that the total page is updated before rendering.
+       * Besides, we want to show the pagination component when changing pages.
+       * So only when browser is fetching data for the first time,
+       * we don't show the pagination component.
+       */}
       {firstFetchDone && (
         <PaginationWrapper
           page={page}
