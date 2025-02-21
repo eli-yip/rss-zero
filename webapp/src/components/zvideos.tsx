fc847fcce10663890eaeca72be79153b54761844
Eli Yip <yezi.2022@outlook.com>
2025-02-21 12:35:57 +0800

    feat(server): use new format for date in zvideo page

--- webapp/src/components/zvideos.tsx
+++ webapp/src/components/zvideos.tsx
@@ -1,4 +1,5 @@
 import { Card, CardBody, Link } from "@heroui/react";
+import moment from "moment-timezone";
 
 import { Zvideo } from "@/types/zvideo";
 
@@ -7,13 +8,19 @@ interface ZvideosProps {
 }
 
 export function Zvideos({ zvideos }: ZvideosProps) {
+  const parseDate = (date: string): string => {
+    const beijingDate = moment.tz(date, "Asia/Shanghai");
+
+    return beijingDate.format("YYYY年MM月DD日");
+  };
+
   return (
     <div className="w-full max-w-3xl gap-4 sm:grid sm:grid-cols-2">
       {zvideos.map((zvideo) => (
         <Card key={zvideo.id} className="mb-4 sm:mb-0">
           <CardBody>
             <div className="flex justify-between gap-4">
-              <p>{zvideo.published_at}</p>
+              <p>{parseDate(zvideo.published_at)}</p>
               <Link isExternal showAnchorIcon href={zvideo.url} target="_blank">
                 {zvideo.title}
               </Link>
