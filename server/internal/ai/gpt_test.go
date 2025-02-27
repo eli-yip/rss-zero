a8e917c8b040cfa717e4468000221168d2afa659
Eli Yip <yezi.2022@outlook.com>
2025-02-27 21:10:53 +0800

    style: fix lint

--- server/internal/ai/gpt_test.go
+++ server/internal/ai/gpt_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestGPT(t *testing.T) {
-	config.InitForTestToml()
+	_ = config.InitForTestToml()
 	assert := assert.New(t)
 	ai := NewAIService(config.C.Openai.APIKey, config.C.Openai.BaseURL)
 	assert.NotNil(ai)
