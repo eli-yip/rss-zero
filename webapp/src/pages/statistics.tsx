0d83afcc4730e5f7259b09c9b8793b7c1a562928
Eli Yip <yezi.2022@outlook.com>
2025-03-24 22:57:20 +0800

    fix(webapp): use correct name for statistics heatmap

--- webapp/src/pages/statistics.tsx
+++ webapp/src/pages/statistics.tsx
@@ -20,7 +20,7 @@ export default function DocsPage() {
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg justify-center text-center">
-          <h1 className={title()}>虚假的数据</h1>
+          <h1 className={title()}>回答热力图</h1>
         </div>
       </section>
 
