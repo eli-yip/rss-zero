157fe2cb618dad07cef440dc1e8b8267560b6f20
Eli Yip <yezi.2022@outlook.com>
2025-04-19 16:41:37 +0800

    chore: add roadmap

--- webapp/src/pages/about.tsx
+++ webapp/src/pages/about.tsx
@@ -1,14 +1,31 @@
+import { Card, CardBody } from "@heroui/react";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+
+import "@/styles/github-markdown.css";
+
+const roadMap = `\
+一份想写但是偷懒没写的路线图...`;
 
 export default function DocsPage() {
 	return (
 		<DefaultLayout>
 			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
 				<div className="inline-block max-w-lg justify-center text-center">
-					<h1 className={title()}>About</h1>
+					<h1 className={title()}>关于本站</h1>
 				</div>
 			</section>
+
+			<div className="mx-auto w-full max-w-3xl">
+				<Card>
+					<CardBody>
+						<Markdown remarkPlugins={[remarkGfm]}>{roadMap}</Markdown>
+					</CardBody>
+				</Card>
+			</div>
 		</DefaultLayout>
 	);
 }
