1ce8baf6077e934370a1a37e8d386f4d71fbf7e8
Eli Yip <yezi.2022@outlook.com>
2026-01-27 10:07:45 +0800

    chore: add more topic to skip

--- pkg/routers/zsxq/parse/topic.go
+++ pkg/routers/zsxq/parse/topic.go
@@ -39,6 +39,7 @@ func (s *ParseService) ParseTopic(topic *models.TopicParseResult, logger *zap.Lo
 		22811844581522481, // Same
 		14588584821842242, // Same
 		14588442484115242, // Same
+		14588445248825242, // Same
 	}
 	if slices.Contains(topicIDSkip, topic.TopicID) {
 		logger.Info("Skip crawling topic, as it will cause markdown parser timeout", zap.Int("topic_id", topic.TopicID))
